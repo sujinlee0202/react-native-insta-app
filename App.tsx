4a3523d22452b2e6812d74d804d01c1ec6f2161e
titch <sujinlee0202@gmail.com>
2024-02-26 23:00:55 +0900

    react navigation을 이용한 라우팅 기능 구현

--- App.tsx
+++ App.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
+import {NavigationContainer} from '@react-navigation/native';
+import Search from './src/screens/Search';
+import Activity from './src/screens/Activity';
+import Profile from './src/screens/Profile';
+import Status from './src/screens/Status';
+import FriendProfile from './src/screens/FriendProfile';
+import EditProfile from './src/screens/EditProfile';
 
-function App(): React.JSX.Element {
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const BottomTabScreen = () => {
   return (
-    <Text>
-      <Home />
-    </Text>
+    <Tab.Navigator
+      screenOptions={() => ({
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          height: 70,
+        },
+      })}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Activity" component={Activity} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Bottom" component={BottomTabScreen} />
+        <Stack.Screen name="Status" component={Status} />
+        <Stack.Screen name="FriendProfile" component={FriendProfile} />
+        <Stack.Screen name="EditProfile" component={EditProfile} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
